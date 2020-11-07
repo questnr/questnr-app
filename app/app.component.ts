763cb9c3cabc02f24a422b3ebbf4e2a4fe0fb366
Brijeshlakkad <brijeshlakkad22@gmail.com>
2020-11-07 16:54:19 +0530

    orientation will be set to portrait and won't be able to change

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as orientation from 'nativescript-orientation';
 import { LoaderService } from './services/loader.service';
 
 @Component({
@@ -8,5 +9,9 @@ import { LoaderService } from './services/loader.service';
 })
 export class AppComponent {
   constructor(public loaderService: LoaderService) {
+    // Set orientation to portrait
+    orientation.setOrientation("portrait");
+    // Disable rotation
+    orientation.disableRotation();
   }
 }
