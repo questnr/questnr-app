105f0c1ff34a234c403d2456f1c37ae8b80d85dd
Brijeshlakkad <brijeshlakkad22@gmail.com>
2020-11-07 16:53:12 +0530

    _variables.ts created. assets for avatar added

---
+++ app/_variables.ts
@@ -0,0 +1,93 @@
+// Colors
+export const qColors = {
+    $primary: "#f15152",
+    $accent: "#9b211f",
+    $heading: "#92140c",
+    $mainBg: "#2a2733",
+    $gray: "#dddddd",
+    $darkGrey: "#d3d5d7",
+    $black: "#242a3a",
+    $white: "#ffffff",
+    $textWhite: "#f9fcfd",
+    $background: "#efefef",
+    $backgroundTransparent: "#efefefaf",
+    $primaryButtonBorder: "#1e555c",
+    $secondaryButtonBorder: "#344966",
+    $mobileNavColor: "#eeeeee",
+
+    $primaryTransparent: "#f8919183",
+    $dataPrimary: "hsla(344, 100%, 44%, 1)",  // #e0003c
+
+    $lightPrimary: "#ffb8b8",
+
+    $grayFont: "rgb(105, 105, 105)",
+    $grayFont2: "#515151",
+    $grayFont3: "#797979",
+    $grayBorder: "#eeeeee",
+    $fixedSections: "#d6d6d6bb",
+
+    $blogText: "#212121",
+    $secondaryFontColor: "rgba(0, 0, 0, 0.7)",
+    $red: "rgb(236, 11, 11)",
+    $danger: "rgba(253, 60, 67, 0.8)",
+    $svgBackground: "#f1515221",
+}
+
+export const qSize = {
+    // Sizes
+    $navHeight: "54px",
+    $btnHeight: "32px",
+    $btnWidth: "112px",
+    $communityNavHeight: "70px",
+
+    // Responsive
+    $desktop: "960px",
+    $tablet: "768px",
+    $mobileLarge: "640px",
+    $mobile: "480px",
+    $mobileSmall: "300px",
+}
+//fonts
+
+export const qFonts = {
+    $defaultFont: "sans-serif",
+
+    //font-color
+    $defaultFontColor: "#767e80",
+
+    // $background-secondary: "#eeeeee",
+    $backgroundSecondary: "#e3e3e3", // rgba(241, 241, 241, 1)
+
+    $linkPrimary: "#1f90f9", // rgb(31, 144, 249
+
+    $linkSecondary: "#7cbffd",
+
+    $linkThird: "#0057a9",
+
+    $hashTag: "#1f90f9", // rgb(31, 144, 249)
+
+    $hashTagBackground: "#f0f8ff",
+
+    $secondaryFontColor: "rgb(156, 156, 156)",
+
+    $inputFieldBackgroundColor: "#f8919183",
+
+    $inputFieldBackgroundColorSecondary: "#f1515221",
+
+    $silver: "silver",
+
+    $required: "#663399",
+
+    $requiredSecondary: "#a985ce",
+
+    /** Font-Family */
+    $fontQuestion: "Palatino",
+    $fontAnswer: "Helvetica Neue",
+
+    $communityTitleFontFamily: "Helvetica Neue",
+    $communityDescriptionFontFamily: "Verdana",
+}
+
+export const qRipple = {
+    $horizontalProfile: qColors.$lightPrimary
+}
