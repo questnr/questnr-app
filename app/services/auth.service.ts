ea92f1b3a87092bb9f3b51aa83b78aea2a190021
Brijeshlakkad <brijeshlakkad22@gmail.com>
2020-11-14 14:39:45 +0530

    Development: loginresponse separated from auth.service

--- app/services/auth.service.ts
+++ app/services/auth.service.ts
@@ -10,27 +10,10 @@ import { LoginResponse } from "~/shared/models/login.model";
 import { LocalUser, LoginUser, User } from "../shared/models/user.model";
 import { JWTService } from "./jwt.service";
 import { LoaderService } from "./loader.service";
+import staticLoginReponse from './login.dev';
 
 const _CURRENT_USER = "_CURRENT_USER";
 const _CURRENT_TOKEN = "_CURRENT_TOKEN";
-
-const staticLoginReponse = {
-  "loginSuccess": true,
-  "accessToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJicmlqZXNobGFra2FkMjIiLCJyb2xlIjpbeyJhdXRob3JpdHkiOiJST0xFX1VTRVIifSx7ImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifV0sImNyZWF0ZWQiOjE2MDQ5NDIxMDI0MzIsIm5hbWUiOiJicmlqZXNobGFra2FkMjIiLCJlbWFpbElkIjoiZHVtbXkxQGR1bW15LnF1ZXN0bnIuY29tIiwiaWQiOjEsImV4cCI6MTYwNTU0NjkwMiwiaWF0IjoxNjA0OTQyMTAyLCJzbHVnIjoiYnJpamVzaGxha2thZDIyLTMwMTk4MjUwMzMifQ.fU8JHooaDOD_NqudiwlYQUXG16nbfZtqm5tU-90c_U362p7cGq8AncgMoUKXP188y0HjnvjbI--YkKyJ4aswGA",
-  "userName": "brijeshlakkad22",
-  "errorMessage": null,
-  "communitySuggestion": false,
-  "firstAttempt": true
-};
-
-const staticLoginReponse2 = {
-  "loginSuccess": true,
-  "accessToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJicmlqZXNobGFra2FkIiwicm9sZSI6W3siYXV0aG9yaXR5IjoiUk9MRV9VU0VSIn0seyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJjcmVhdGVkIjoxNjA0Njg0MDU1NTIwLCJuYW1lIjoiQnJpamVzaCBMYWtrYWQiLCJlbWFpbElkIjoiYnJpamVzaGxha2thZDIyQGdtYWlsLmNvbSIsImlkIjoxLCJleHAiOjE2MDUyODg4NTUsImlhdCI6MTYwNDY4NDA1NSwic2x1ZyI6ImJyaWplc2gtbGFra2FkLTA0ODc1MTA2NzYifQ.nJCw6qNrEPn8CnLOEt4YZRcrpuFYM_t9hs6ipnEozyxBiRCJ2rI6yejVww8Y4mRgNKdD_XjCsHR-ypwAlv9xLg",
-  "userName": "brijeshlakkad",
-  "errorMessage": null,
-  "communitySuggestion": false,
-  "firstAttempt": true
-};
 
 @Injectable()
 export class AuthService {
@@ -42,10 +25,12 @@ export class AuthService {
     private jwtService: JWTService,
     private loaderService: LoaderService,
     private routerExtension: RouterExtensions) {
-    // @todo: only for testing purpose
-    this.accessToken = staticLoginReponse.accessToken;
-    this.user = JSON.stringify(staticLoginReponse.accessToken);
 
+    if (!environment.production) {
+      // @todo: only for testing purpose
+      this.accessToken = staticLoginReponse.accessToken;
+      this.user = JSON.stringify(staticLoginReponse.accessToken);
+    }
     // this.getLoggedInUserDetails().subscribe((user) => {
     //   console.log("getLoggedInUserDetails", user);
     // });
---
+++ app/services/login.dev.ts
@@ -0,0 +1,19 @@
+const staticLoginReponse = {
+    "loginSuccess": true,
+    "accessToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJicmlqZXNobGFra2FkMjIiLCJyb2xlIjpbeyJhdXRob3JpdHkiOiJST0xFX1VTRVIifSx7ImF1dGhvcml0eSI6IlJPTEVfQURNSU4ifV0sImNyZWF0ZWQiOjE2MDQ5NDIxMDI0MzIsIm5hbWUiOiJicmlqZXNobGFra2FkMjIiLCJlbWFpbElkIjoiZHVtbXkxQGR1bW15LnF1ZXN0bnIuY29tIiwiaWQiOjEsImV4cCI6MTYwNTU0NjkwMiwiaWF0IjoxNjA0OTQyMTAyLCJzbHVnIjoiYnJpamVzaGxha2thZDIyLTMwMTk4MjUwMzMifQ.fU8JHooaDOD_NqudiwlYQUXG16nbfZtqm5tU-90c_U362p7cGq8AncgMoUKXP188y0HjnvjbI--YkKyJ4aswGA",
+    "userName": "brijeshlakkad22",
+    "errorMessage": null,
+    "communitySuggestion": false,
+    "firstAttempt": true
+};
+
+const staticLoginReponse2 = {
+    "loginSuccess": true,
+    "accessToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJicmlqZXNobGFra2FkIiwicm9sZSI6W3siYXV0aG9yaXR5IjoiUk9MRV9VU0VSIn0seyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJjcmVhdGVkIjoxNjA1MzQ0ODMyMzAxLCJuYW1lIjoiQnJpamVzaCBMYWtrYWQiLCJlbWFpbElkIjoiYnJpamVzaGxha2thZDIyQGdtYWlsLmNvbSIsImlkIjoxLCJleHAiOjE2MDU5NDk2MzIsImlhdCI6MTYwNTM0NDgzMiwic2x1ZyI6ImJyaWplc2gtbGFra2FkLTA0ODc1MTA2NzYifQ.gnKA8-XLMNi3iRdJWGqeW1JwfEXH41KiG2hIy_EMwTjzggI7bbBgV250GnktTNAZt9SPUrw8lEHfCJX_gSFQrA",
+    "userName": "brijeshlakkad",
+    "errorMessage": null,
+    "communitySuggestion": false,
+    "firstAttempt": true
+};
+
+export default staticLoginReponse;
